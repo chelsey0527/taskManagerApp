171978968d05e44bae7dbb47088273d37889dd99
Chelsey Lee <c.lee@nombolo.com>
2024-07-11 15:00:36 -0700

    Create firestore service for task

--- services/firestore.ts
+++ services/firestore.ts
@@ -1,25 +1,61 @@
-import {collection, addDoc, getDocs, query, where} from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+} from 'firebase/firestore';
 import {db} from '../config/firebase-config';
 import {Task} from '../types/task';
+
+// Function to get tasks for a user
+export const getUserTasks = async (userId: string): Promise<Task[]> => {
+  try {
+    const tasks: Task[] = [];
+    const q = query(collection(db, 'tasks'), where('userId', '==', userId));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(doc => {
+      tasks.push({id: doc.id, ...doc.data()} as Task);
+    });
+    return tasks;
+  } catch (error) {
+    console.error('Error fetching tasks: ', error);
+    throw error;
+  }
+};
 
 // Function to add a task
 export const addTask = async (task: Omit<Task, 'id'>) => {
   try {
-    const docRef = await addDoc(collection(db, 'tasks'), task);
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
+    await addDoc(collection(db, 'tasks'), task);
+  } catch (error) {
+    console.error('Error adding task: ', error);
+    throw error;
   }
 };
 
-// Function to get tasks for a user
-export const getUserTasks = async (userId: string): Promise<Task[]> => {
-  const q = query(collection(db, 'tasks'), where('userId', '==', userId));
-  const querySnapshot = await getDocs(q);
-  const tasks: Task[] = [];
-  querySnapshot.forEach(doc => {
-    const taskData = doc.data() as Omit<Task, 'id'>;
-    tasks.push({id: doc.id, ...taskData});
-  });
-  return tasks;
+// Function to update a task
+export const updateTask = async (id: string, task: Partial<Task>) => {
+  try {
+    const taskRef = doc(db, 'tasks', id);
+    await updateDoc(taskRef, task);
+  } catch (error) {
+    console.error('Error adding task: ', error);
+    throw error;
+  }
+};
+
+// Function to delete a task
+export const deleteTask = async (id: string) => {
+  try {
+    const taskRef = doc(db, 'tasks', id);
+    await deleteDoc(taskRef);
+  } catch (error) {
+    console.error('Error deleting task: ', error);
+    throw error;
+  }
 };
