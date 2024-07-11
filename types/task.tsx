c7336c0b4e34fb0594d888948003da409790a3be
Chelsey Lee <c.lee@nombolo.com>
2024-07-11 13:22:38 -0700

    Create task interface

--- types/task.tsx
+++ types/task.tsx
@@ -1,6 +1,10 @@
 export interface Task {
   id: string;
   name: string;
+  category: string;
+  createdTime: number;
   description: string;
+  deadline: number;
+  completed: boolean;
   userId: string;
 }
